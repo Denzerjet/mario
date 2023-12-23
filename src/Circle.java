c9be1e958369b8bd0918d4513064067020eb2787
Kian Nowrouzi <Ryan.Nowrouzi@gmail.com>
2023-12-23 14:22:01 -0800

    moveable red ball

--- src/Circle.java
+++ src/Circle.java
@@ -1,24 +1,100 @@
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Circle extends JPanel {
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+public class Circle extends JPanel implements KeyListener {
+    private int circleX = 25;
+    private int circleY = 435;
 
-        // Set the color and draw a filled circle
-        g.setColor(Color.BLUE);
-        g.fillOval(100, 100, 100, 100); // Parameters: x, y, width, height
+    private boolean moveUp = false;
+    private boolean moveDown = false;
+    private boolean moveRight = false;
+    private boolean moveLeft = false;
+
+    public Circle() {
+        setFocusable(true); // allow JPanel to receive keyboard focus
+        addKeyListener(this); // register the JPanel as a keylistener
+        setFocusTraversalKeysEnabled(false); // allow simult input
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Circle Drawing Example");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(2000, 1050);
+   @Override
+   protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
-        Circle circlePanel = new Circle();
-        frame.add(circlePanel);
+       // set color and draw
+       g.setColor(Color.RED);
+       g.fillOval(circleX, circleY, 25, 25);
+   }
+
+   @Override
+   public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+       if (key == KeyEvent.VK_W) {
+           moveUp = true;
+       } else if (key == KeyEvent.VK_S) {
+           moveDown = true;
+       } else if (key == KeyEvent.VK_A) {
+           moveLeft = true;
+       } else if (key == KeyEvent.VK_D) {
+           moveRight = true;
+       }
+
+        // update drawing
+       updateCirclePosition();
+   }
+
+   @Override
+   public void keyTyped(KeyEvent e) {
+        // do nothing
+   }
+
+   @Override
+   public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+
+       if (key == KeyEvent.VK_W) {
+           moveUp = false;
+       } else if (key == KeyEvent.VK_S) {
+           moveDown = false;
+       } else if (key == KeyEvent.VK_A) {
+           moveLeft = false;
+       } else if (key == KeyEvent.VK_D) {
+           moveRight = false;
+       }
+
+       updateCirclePosition();
+   }
+
+   private void updateCirclePosition() {
+        if (moveUp && !moveDown) {
+            circleY -= 10;
+        } else if (moveDown && !moveUp) {
+            circleY += 10;
+        }
+
+        if (moveLeft && !moveRight) {
+            circleX -= 10;
+        } else if (moveRight && !moveLeft) {
+            circleX += 10;
+        }
+
+        repaint();
+   }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("mario_level");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+
+
+        // create the frame
+        Circle circle = new Circle();
+        frame.add(circle);
 
         frame.setVisible(true);
+
+
     }
 }
